7fba87d300a63bb42100d56499e7a68077415bf0
Adam Desivi <desivi@desivi.com>
2021-05-08 20:49:10 +0200

    feat: add equals method on ValueNumber

--- test/unit/value-number.test.ts
+++ test/unit/value-number.test.ts
@@ -2,6 +2,9 @@ import { expect } from 'chai';
 import { ValueNumber } from '../../src/value-number';
 
 describe('ValueNumber', () => {
+  class MyValue extends ValueNumber {}
+  class MyOtherValue extends ValueNumber {}
+
   it('extends Number', () => {
     expect(ValueNumber.prototype).to.be.instanceof(Number);
   });
@@ -12,5 +15,25 @@ describe('ValueNumber', () => {
 
   it('ensures that value can be compared falsy', () => {
     expect(new ValueNumber(2)).not.to.be.eql(new ValueNumber(3));
+  });
+
+  describe('comparison', () => {
+    describe('equality', () => {
+      it('is not equal if compared with a null value', () => {
+        expect(new MyValue(1).equals(null)).to.be.false;
+      });
+
+      it('returns true if both instance are equal', () => {
+        expect(new MyValue(1).equals(new MyValue(1))).to.be.true;
+      });
+
+      it('returns false if one instance is different from other by values', () => {
+        expect(new MyValue(1).equals(new MyValue(2))).to.be.false;
+      });
+
+      it('returns false if one instance has different type then other', () => {
+        expect(new MyValue(1).equals(new MyOtherValue(1))).to.be.false;
+      });
+    });
   });
 });
