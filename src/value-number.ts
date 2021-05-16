86ac709a4decf5c0604145e6af84ee5cb1fe07e4
Adam Desivi <desivi@desivi.com>
2021-05-16 17:41:11 +0200

    fix: apply HookableMixin to ValueNumber

--- src/value-number.ts
+++ src/value-number.ts
@@ -1,9 +1,44 @@
 import util from 'util';
 import { ValueTransformer } from 'typeorm';
+import { getTypeName } from '@eveble/helpers';
+import { EvebleTypes, kernel, HookableMixin } from '@eveble/eveble';
 
 export class ValueNumber extends Number {
   constructor(value: number) {
     super(value);
+
+    this.onValidation(value);
+
+    Object.defineProperties(this, {
+      // HookableMixin
+      registerHook: {
+        enumerable: false,
+      },
+      overrideHook: {
+        enumerable: false,
+      },
+      getHook: {
+        enumerable: false,
+      },
+      getHookOrThrow: {
+        enumerable: false,
+      },
+      getHooks: {
+        enumerable: false,
+      },
+      getActions: {
+        enumerable: false,
+      },
+      hasHook: {
+        enumerable: false,
+      },
+      hasAction: {
+        enumerable: false,
+      },
+      removeHook: {
+        enumerable: false,
+      },
+    });
   }
 
   public toPlainObject(): number {
@@ -35,6 +70,90 @@ export class ValueNumber extends Number {
   }
 
   /**
+   * EjsonableMixin
+   */
+  /**
+   * @alias getTypeName
+   * @remarks
+   * Compatibility for EJSON serializer: `@eveble/ejson`
+   */
+  public typeName(): EvebleTypes.TypeName {
+    return this.getTypeName();
+  }
+
+  /**
+   * @alias getTypeName
+   * @remarks
+   * Compatibility for EJSON serializer: `@eveble/ejson`
+   */
+  public static typeName(): EvebleTypes.TypeName {
+    return this.getTypeName();
+  }
+
+  /**
+   * SerializableMixin
+   */
+  /**
+   * Returns definable type name.
+   * @returns Type name as a string.
+   */
+  public getTypeName(): EvebleTypes.TypeName {
+    return getTypeName(this) as EvebleTypes.TypeName;
+  }
+
+  /**
+   * Returns definable type name
+   * @returns Type name as a string.
+   */
+  public static toString(): EvebleTypes.TypeName {
+    return this.getTypeName();
+  }
+
+  /**
+   * Returns definable type name.
+   * @returns Type name as a string.
+   */
+  public static getTypeName(): EvebleTypes.TypeName {
+    return getTypeName(this) as EvebleTypes.TypeName;
+  }
+
+  /**
+   * Serializes value into a JSON-compatible value. It preserves all custom
+   * field types, however the initial value type is not saved.
+   * @returns Normalized value as JSON-compatible without type identifers.
+   */
+  public toJSONValue(): Record<string, any> {
+    return kernel.serializer?.toJSONValue(this);
+  }
+
+  /**
+   * HookableMixin
+   * @remarks
+   * Pull methods definitions from HookableMixin.
+   * Accessing them via:
+   * `ValueString.prototype.registerHook(...)`
+   * will be still unavailable, for that we assign below again functions
+   * to prototype.
+   */
+  registerHook = HookableMixin.prototype.registerHook;
+
+  overrideHook = HookableMixin.prototype.overrideHook;
+
+  getHook = HookableMixin.prototype.getHook;
+
+  getHookOrThrow = HookableMixin.prototype.getHookOrThrow;
+
+  getHooks = HookableMixin.prototype.getHooks;
+
+  getActions = HookableMixin.prototype.getActions;
+
+  hasHook = HookableMixin.prototype.hasHook;
+
+  hasAction = HookableMixin.prototype.hasAction;
+
+  removeHook = HookableMixin.prototype.removeHook;
+
+  /**
    * Returns `@Column` transformer for TypeORM.
    */
   public static transformer = function (): ValueTransformer {
@@ -52,4 +171,84 @@ export class ValueNumber extends Number {
       },
     };
   };
+
+  /**
+   * On validation hook.
+   * @param value - Value as a `number`.
+   * @param isStrict - Flag indicating that validation should be done in strict mode.
+   * @returns Returns `true` on valid value, else throws.
+   * @throws {Error}
+   * Thrown if the provided value does not match the validation.
+   */
+  protected onValidation(value: number, isStrict = true): boolean {
+    if (!kernel.isValidating()) {
+      return true;
+    }
+    try {
+      kernel.validator.validate(value, Number, isStrict);
+    } catch (error) {
+      const { message } = error;
+      const typeName: EvebleTypes.TypeName = this.getTypeName();
+      throw new error.constructor(`${typeName}: ${message}`);
+    }
+
+    const hooks: EvebleTypes.hooks.Mappings = this.getHooks('onValidation');
+    for (const hook of Object.values(hooks)) {
+      hook.bind(this)(value);
+    }
+
+    return true;
+  }
+
+  /**
+   * ValidableMixin
+   */
+  /**
+   * Sets validator on `ValueObject` in form of `onValidation` hook.
+   * @param validator - Function validating ValueObject properties.
+   */
+  public static setValidator(validator: (...args: any[]) => boolean): void {
+    (this.prototype as any).overrideHook(
+      'onValidation',
+      'validation',
+      validator
+    );
+  }
+
+  /**
+   * Returns `ValueObject` validator from `onValidation` hook.
+   * @returns Validation `Function` if assigned, else `undefined`.
+   */
+  public static getValidator(): () => boolean {
+    return (this.prototype as any).getHook('onValidation', 'validation');
+  }
+
+  /**
+   * Removes validation from `onValidation` hook.
+   */
+  public static removeValidator(): void {
+    (this.prototype as any).removeHook('onValidation', 'validation');
+  }
+
+  /**
+   * Evaluates if validator is assigned to `ValueObject`
+   * @returns Returns `true` if validator is assigned, else `false`.
+   */
+  public static hasValidator(): boolean {
+    return (this.prototype as any).hasHook('onValidation', 'validation');
+  }
 }
+
+/**
+ * HookableMixin
+ */
+const proto = ValueNumber.prototype as any;
+proto.registerHook = HookableMixin.prototype.registerHook;
+proto.overrideHook = HookableMixin.prototype.overrideHook;
+proto.getHook = HookableMixin.prototype.getHook;
+proto.getHookOrThrow = HookableMixin.prototype.getHookOrThrow;
+proto.getHooks = HookableMixin.prototype.getHooks;
+proto.getActions = HookableMixin.prototype.getActions;
+proto.hasHook = HookableMixin.prototype.hasHook;
+proto.hasAction = HookableMixin.prototype.hasAction;
+proto.removeHook = HookableMixin.prototype.removeHook;
--- test/unit/value-number.test.ts
+++ test/unit/value-number.test.ts
@@ -1,7 +1,16 @@
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import { kernel, UnmatchedTypeError } from '@eveble/eveble';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
 import { ValueNumber } from '../../src/value-number';
 
+chai.use(sinonChai);
+
 describe('ValueNumber', () => {
+  beforeEach(() => {
+    kernel.enableValidation();
+  });
+
   class MyValue extends ValueNumber {}
   class MyOtherValue extends ValueNumber {}
 
@@ -53,6 +62,69 @@ describe('ValueNumber', () => {
         expect(result).to.be.instanceof(MyValue);
         expect(result).to.be.eql(new MyValue(1));
       });
+    });
+  });
+
+  describe('validation', () => {
+    afterEach(() => {
+      ValueNumber.prototype.removeHook('onValidation', 'first');
+      ValueNumber.prototype.removeHook('onValidation', 'second');
+    });
+
+    it('throws UnmatchedTypeError when provide type is not a number', () => {
+      expect(() => new ValueNumber('foo' as any)).to.throw(
+        UnmatchedTypeError,
+        `Expected String("foo") to be a Number`
+      );
+    });
+
+    it('ensures that errors are only thrown upon enabled validation on kernel', () => {
+      kernel.disableValidation();
+
+      expect(() => new ValueNumber('foo' as any)).to.not.throw(Error);
+    });
+
+    it('iterates over registered onValidation hooks', () => {
+      const firstValidator = sinon.stub();
+      const secondValidator = sinon.stub();
+
+      ValueNumber.prototype.registerHook(
+        'onValidation',
+        'first',
+        firstValidator
+      );
+      ValueNumber.prototype.registerHook(
+        'onValidation',
+        'second',
+        secondValidator
+      );
+
+      const value = 1;
+      new ValueNumber(value);
+      expect(firstValidator).to.be.calledOnce;
+      expect(firstValidator).to.be.calledWith(value);
+      expect(secondValidator).to.be.calledOnce;
+      expect(secondValidator).to.be.calledWith(value);
+    });
+
+    it('ensures that error is thrown upon invalid value', () => {
+      const firstValidator = sinon.stub();
+      const secondValidator = sinon.stub();
+
+      ValueNumber.prototype.registerHook(
+        'onValidation',
+        'first',
+        firstValidator
+      );
+      ValueNumber.prototype.registerHook(
+        'onValidation',
+        'second',
+        secondValidator
+      );
+      const error = new Error('invalid-value');
+      secondValidator.throws(error);
+
+      expect(() => new ValueNumber(1)).to.throw(error);
     });
   });
 });
